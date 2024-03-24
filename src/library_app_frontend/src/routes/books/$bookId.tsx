6d103fd285b4bd75dab1972d132d2460b3d84154
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-24 13:16:16 -0500

    feat: prepare book detail route

--- src/library_app_frontend/src/routes/books/$bookId.tsx
+++ src/library_app_frontend/src/routes/books/$bookId.tsx
@@ -1,15 +1,27 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
+const BookPage = () => {
+  const data = Route.useLoaderData();
+  return (
+    <pre>
+      <code>{JSON.stringify(data, null, 2)}</code>
+    </pre>
+  );
+};
+
 export const Route = createFileRoute('/books/$bookId')({
-  beforeLoad: async ({ params }) => {
+  beforeLoad: async ({ params, context }) => {
+    if (!context.auth.isConnected && !context.auth.isInitializing) {
+      // Needs sign in
+      throw redirect({ to: '/' });
+    }
     if (typeof params.bookId === 'undefined') {
+      // No book id
       throw redirect({ to: '/' });
     }
   },
   loader: async ({ params }) => {
     return { book: params.bookId }; // fetchPost(params.postId);
   },
-  component: () => {
-    return <p>Book</p>;
-  },
+  component: BookPage,
 });
