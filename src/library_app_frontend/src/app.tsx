73a385629298e345687a80f2c2d7a1bac4fc675a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-24 12:21:24 -0500

    fix: login page not showing

--- src/library_app_frontend/src/app.tsx
+++ src/library_app_frontend/src/app.tsx
@@ -1,10 +1,11 @@
 import { createClient } from '@connect2ic/core';
 import { RouterProvider } from '@tanstack/react-router';
 import { InternetIdentity } from '@connect2ic/core/providers/internet-identity';
-// import * as library_app_backend from '@/backend/index';
-import * as library_app_backend from './../../declarations/library_app_backend';
+import * as library_app_backend from '@/backend';
 import { Connect2ICProvider, useConnect } from '@connect2ic/react';
 import { router } from './router';
+
+const browser: 'safari' | 'others' = 'others';
 
 const client = createClient({
   canisters: {
@@ -13,7 +14,10 @@ const client = createClient({
   },
   providers: [
     new InternetIdentity({
-      providerUrl: 'http://127.0.0.1:8000/?canisterId=bkyz2-fmaaa-aaaaa-qaaaq-cai',
+      providerUrl:
+        browser === 'others'
+          ? 'http://bkyz2-fmaaa-aaaaa-qaaaq-cai.localhost:8000/'
+          : 'http://127.0.0.1:8000/?canisterId=bkyz2-fmaaa-aaaaa-qaaaq-cai',
       // dev: true,
     }),
   ],
