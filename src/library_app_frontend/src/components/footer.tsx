2fabad2c06063f2b27bf4eb6409aecbc856e72d1
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-03-24 22:22:17 -0500

    chore: update footer

--- src/library_app_frontend/src/components/footer.tsx
+++ src/library_app_frontend/src/components/footer.tsx
@@ -9,7 +9,10 @@ export const Footer = () => {
         <p className={'pt-0.5'}>Created by</p>
         <ul className={'flex flex-row items-center gap-1.5'}>
           <li>
-            <a title={'Jahir Fiquitiva'} href={'https://bio.jahir.dev'} target={'_blank'}>
+            <a
+              title={'Jahir Fiquitiva'}
+              href={'https://bio.jahir.dev'}
+              target={'_blank'}>
               <img
                 src={`https://unavatar.io/jahirfiquitiva?fallback=https://source.boringavatars.com/beam/28/jahirfiquitiva`}
                 loading={'lazy'}
@@ -27,21 +30,6 @@ export const Footer = () => {
               target={'_blank'}>
               <img
                 src={`https://unavatar.io/christianrv29?fallback=https://source.boringavatars.com/beam/28/christianrv29`}
-                loading={'lazy'}
-                decoding={'async'}
-                width={28}
-                height={28}
-                className={'rounded-full bg-emerald-400'}
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              title={'Carlos Medina Riaño'}
-              href={'https://www.linkedin.com/in/carlos-medinar/'}
-              target={'_blank'}>
-              <img
-                src={`https://unavatar.io/carlos-medinar?fallback=https://source.boringavatars.com/beam/28/carlos-medinar`}
                 loading={'lazy'}
                 decoding={'async'}
                 width={28}
